3c87d940d492e6b238a5331994307a351014a010
Ian Brennan <ian.brennan@icloud.com>
2017-07-17 17:07:03 -0600

    trying gpio poll multiple pins

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ const camera = new RaspiCam({ mode: 'photo', ouput: './photos' })
 function pollcb(pin) {
   var state = rpio.read(pin) ? 'pressed' : 'released'
   console.log(`button event on ${pin} currently ${state}`)
-  camera.start()
 }
 
 var init = function() {
   console.log('initializing')
-  rpio.open(17, rpio.INPUT, rpio.PULL_DOWN)
+  rpio.open(16, rpio.INPUT, rpio.PULL_DOWN)
+  rpio.open(6, rpio.INPUT, rpio.PULL_DOWN)
+  rpio.open(13, rpio.INPUT, rpio.PULL_DOWN)
+  rpio.open(26, rpio.INPUT, rpio.PULL_DOWN)
+  rpio.poll(16, pollcb)
+  rpio.poll(6, pollcb)
+  rpio.poll(13, pollcb)
+  rpio.poll(26, pollcb)
 }
 
 init()
