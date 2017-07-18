9c1a42f63503eb80728ca0e5edc0874bc88c1543
ipbrennan90 <ian.brennan@icloud.com>
2017-07-18 16:20:08 +0000

    camera ta timelapse

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 const rpio = require('rpio')
 const RaspiCam = require('raspicam')
-const camera = new RaspiCam({ mode: 'photo', ouput: './photos' })
+const camera = new RaspiCam({
+  mode: "timelapse", 
+  output: "./photos/image_%06d.jpg",
+  encoding: "jpg",
+  timelapse: 1000,
+  timeout: 5000
+})
 
 function pollcb(pin) {
   var state = rpio.read(pin) ? 'pressed' : 'released'
   console.log(`button event on ${pin} currently ${state}`)
+  if (state === 'pressed') {
+    camera.start()
+  }
 }
 
 var init = function() {
   console.log('initializing')
   rpio.open(16, rpio.INPUT, rpio.PULL_DOWN)
-  rpio.open(6, rpio.INPUT, rpio.PULL_DOWN)
-  rpio.open(13, rpio.INPUT, rpio.PULL_DOWN)
-  rpio.open(26, rpio.INPUT, rpio.PULL_DOWN)
   rpio.poll(16, pollcb)
-  rpio.poll(6, pollcb)
-  rpio.poll(13, pollcb)
-  rpio.poll(26, pollcb)
+  camera.on('start', function(err, timestamp) {
+    console.log("photo started at " + timestamp)
+  })
+  camera.on('read', function(err, timestamp, filename) {
+    console.log("photo captured with filename: " + filename)
+  })
+  camera.on('exit', function( timestamp ){
+    console.log("photo child process has exited at " + timestamp )
+  })
 }
 
 init()
